2ead32f5f1a9f03341e4bc013172f26aeee029e5
NachoGuidali <nachog.akd@hotmail.com>
2024-04-19 15:47:42 -0300

    vercel config

--- ferreteriaDjango/settings.py
+++ ferreteriaDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qwf^^m8g#(^s)7b3_4+f+veyk5u7g!iq&whkjeeovb+krdu8v5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '9c0c-186-138-6-68.ngrok-free.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '9c0c-186-138-6-68.ngrok-free.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -118,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "web/static")]
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "web/staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
